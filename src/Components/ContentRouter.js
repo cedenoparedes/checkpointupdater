5dcbff6ae3e71b78611ccc5db316e6002aa8c603
Eber <cedenoparedes04@gmail.com>
2020-10-29 12:21:10 -0400

    React Router Implementation

---
+++ src/Components/ContentRouter.js
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import NoFound from "./NoFound";
+// import GlobalContext from "../context/globalcontext";
+
+const ContentRouter = () => {
+//   const [, , contextMiddleware] = useContext(GlobalContext);
+//   useEffect(() => {}, []);
+
+  return (
+    <div id="main-container">
+      <Switch>
+        <Route exact path="/">
+            <Login />
+        </Route>
+        <Route path="*">
+          <NoFound />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default ContentRouter;
