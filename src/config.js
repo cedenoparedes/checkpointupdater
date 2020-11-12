c81ec5387de868c6415e3c00d18c4149e7c57067
Luis David Roque Cabreja <Davidroque21@hotmail.com>
2020-11-12 02:01:34 -0400

    Login Screen doesn't have scroll now and media queries in img has been fixed.
    The carnet image has been fixed and now is resizing well in all of the sizes, also scroll has been removed.

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const enviroments = { dev: "dev", prod: "prod" };
 module.exports = {
   env: enviroments.dev,
   baseApiURL: {
-    dev: "http://61aefe86bcac.ngrok.io",
+    dev: "http://cee5cf1c78ae.ngrok.io",
     prod: "",
   },
 };
