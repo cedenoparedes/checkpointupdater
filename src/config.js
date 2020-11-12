e9ff9076330535f5d2c6a9dca9794064ad3e34be
Luis David Roque Cabreja <Davidroque21@hotmail.com>
2020-11-12 10:06:31 -0400

    Media Queries for menu has been improved

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const enviroments = { dev: "dev", prod: "prod" };
 module.exports = {
   env: enviroments.dev,
   baseApiURL: {
-    dev: "http://cee5cf1c78ae.ngrok.io",
+    dev: "http://c7128af11493.ngrok.io",
     prod: "",
   },
 };
