498041bcd0a1176ee84d36495c2c5868cef4af8f
David Roque <davidroque21@hotmail.com>
2020-12-08 22:50:36 -0400

    fixed horizontal scroll in screens

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
 
 
     return (
-        <footer className="Footer">
+        <footer className="Footer container-fluid">
             <div className="row">
                 <div className="col-6 footer-style pl-2 pt-1 d-flex text-center align-items-center">
                     <figure className="location-img">
