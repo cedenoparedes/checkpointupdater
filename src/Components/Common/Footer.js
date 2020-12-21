cced493a8112fc23660e2cdae7f12e65c9d4a523
David Roque <davidroque21@hotmail.com>
2020-12-21 18:44:52 -0400

    changes in electron.js

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -1,30 +1,15 @@
 import React from 'react'
 import images from './Images/index'
-const { ipcRenderer } = window.require("electron");
+
 
 const Footer = () => {
+
 
     const jpson = require('../../../package.json');
     const versionString = jpson.version
     const version = <p id="version-text"> V: {versionString}</p>
 
-   
-    const notification = document.getElementById('notification');
-    const message = document.getElementById('message');
-    const restartButton = document.getElementById('restart-button');
 
-    ipcRenderer.on('update_available', () => {
-    ipcRenderer.removeAllListeners('update_available');
-    message.innerText = 'A new update is available. Downloading now...';
-    notification.classList.remove('hidden');
-    });
-    
-    ipcRenderer.on('update_downloaded', () => {
-    ipcRenderer.removeAllListeners('update_downloaded');
-    message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
-    restartButton.classList.remove('hidden');
-    notification.classList.remove('hidden');
-    });
 
 
     return (
@@ -40,15 +25,7 @@ const Footer = () => {
                     <spam> {version}</spam>
                 </div>
             </div>
-            <div id="notification" class="hidden">
-                <p id="message"></p>
-                <button id="close-button" onClick="closeNotification()">
-                        Close
-                </button>
-                <button id="restart-button" onClick="restartApp()" class="hidden">
-                        Restart
-                </button>
-            </div>
+
         </footer>
 
     )
